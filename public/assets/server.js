d15a4ef80d70a6733d288a2187d6e35159087750
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-01-14 17:53:59 -0600

    Connect to server and got insomnia to work

--- public/assets/server.js
+++ public/assets/server.js
@@ -1,23 +1,90 @@
-// Import express
 const express = require('express');
-// Require the JSON file and assign it to a variable called 'noteData'
-const noteData = require('./db/db.json');
+// import cors from "cors";
+// import noteData from "../db/db.json" assert { type: "json" };
 
-// Initialize app variable by setting it to the value of express()
+// Start express
 const app = express();
-const PORT = 3002;
 
+// Set up port
+const http = require("http");
+const PORT = process.env.PORT || 3001;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" })
+    res.end('Hello world!')
+})
+
+server.listen(PORT, () => console.log(`server started on port ${PORT};` + 'press Ctrl-C to terminate....'));
+   
+// Use UUID to generate unique IDs for each note
+const { v4: uuidv4 } = require('uuid');
+
+// Use the db.json on back-end to store and retrieve notes using the fs module
+// Promise version of fs.readFile
+// const readFromFile = promisify(fs.readFile);
+
+// const writeToFile = (destination, content) => {
+//     fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => {
+//         err ? console.error(err) : console.info(`\nData written to ${destination}`);
+//     });
+// };
+
+// const readAndAppend = (content, file) => {
+//     fs.readFile(file, 'UTF8', (err, data) => {
+//         if (err) {
+//             console.error(err);
+//             } else {
+//                 const parsedData = JSON.parse(data);
+//                 parsedData.push(content);
+//                 writeToFile(file, parsedData);
+//                 }
+//             });
+//         };
+
+// module.exports = { readFromFile, writeToFile, readAndAppend };
+
+
+            
+// HTML routes
+// TODO: Create a GET /notes route that returns notes.html
+// app.get('/notes', (req, res) => {
+//     res.sendFile(join(__dirname, '../notes.html'));
+// });
+
+// TODO: Create a GET * route that returns index.html
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.html'));
+// });
+
+// API routes
+// TODO: Create a GET /api/notes route that reads the db.json file and returns all saved notes as JSON
+// app.get('/api/notes', (req, res) => {
+//     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+// });
+
+// TODO: Create a POST /api/notes route that receives a new note to save on the request body, adds it to the db.json file, and then returns the new note to the client
+// TODO: Give each note a unique id when it's saved (using uuid)
+
+// TODO: BONUS: Create a DELETE /api/notes/:id route that receives a query parameter containing the id of a note to delete.
+// TODO: In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+// express()
+//     .use(express.static('public'))
+//     .set('views', 'views')
+//     .set('view engine', 'ejs')
+//     .get('/', (req, res) => res.render('pages/index'))
+//     .get('/send', (req, res) => res.render('pages/send'))
+//     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 // Invoke middleware, app.use() to serve static files from the public folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => {res.send('Hello World!')});
 
 // res.json() allows to return JSON instead of a buffer, string, or static file
-app.get('/api', (req, res) => res.json(noteData));
+// app.get('/api', (req, res) => res.json(noteData));
 
 // app.get('/send', (req, res) => res.sendFile(path.join(__dirname, 'public/send.html')));
 
 // Added a HTML route that will serve up the paths.html file
 // app.get('/paths', (req, res) => res.sendFile(path.join(__dirname, 'public/paths.html')));
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+// app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`)});
